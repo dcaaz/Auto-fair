adceb41f6d9de0e5b3a31aed0e13de8333f1560a
dcaaz <danicorreaazevedo@gmail.com>
2023-02-03 09:42:37 -0300

    feat: delete index

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import express, { json, Request, Response } from "express";
-import fruitsRouter from "./routers/fruits-router";
-
-const app = express();
-app.use(json());
-
-app.get("/health", (req: Request, res: Response) => res.send("I'am alive!"));
-app.use(fruitsRouter);
